29b6c4a5d63641657b7e00ec9d9c8a5d6d5a5611
jimbrew <0703983@rgu.ac.uk>
2018-02-26 20:57:52 +0000

    updated the remove/add features

--- lab7/contentlist.js
+++ lab7/contentlist.js
@@ -1,20 +1,35 @@
+// function addContent() {
+//   // add a list of items to the content div
+//   var items = ["hewey", "dewey", "louie"];
+//
+//   // build the html string for a <ul> list
+//   var items_html = "<div>";
+//   for (var i = 0; i < items.length; i++) {
+//     item = items[i];
+//     items_html += "<p>" + item + "</p>";
+//   };
+//   items_html += "</div>";
+//
+//   // using javascript
+//   // 1. find the content div
+//   // 2. modify its html attribute by adding items_html
+//   var list = document.getElementById("content");
+//   list.innerHTML = items_html;
+//
+// }
 
-function addContent () {
-	// add a list of items to the content div
-	var items = ["hewey", "dewey", "louie"];
+function addItems() {
+  var div = document.getElementById("content");
+  var input = document.getElementById("input");
+  var paragraph = document.createElement("p");
+  paragraph.setAttribute('id', input.value);
+  paragraph.appendChild(document.createTextNode(input.value));
+  div.appendChild(paragraph);
+}
 
-	// build the html string for a <ul> list
-	var items_html = "<div>";
-	for (var i=0; i < items.length; i++) {
-		item = items[i];
-		items_html += "<p>" + item + "</p>";
-	};
-	items_html += "</div>";
-
-	// using javascript
-	// 1. find the content div
-	// 2. modify its html attribute by adding items_html
-	var list = document.getElementById("content");
-	list.innerHTML = items_html;
-
-	}
+function removeItems() {
+  var div = document.getElementById("content");
+  var input = document.getElementById("input");
+  var item = document.getElementById(input.value);
+  div.removeChild(item);
+}
