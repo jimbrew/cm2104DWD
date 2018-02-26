cc788d290d04161ef84247dff77fd3c28af0ea7a
JAMES STEVENSON <0703983@rgu.ac.uk>
2018-02-26 18:09:50 +0000

    add items/remove to list using buttons

--- lab7/contentlist.js
+++ lab7/contentlist.js
@@ -2,18 +2,19 @@
 function addContent () {
 	// add a list of items to the content div
 	var items = ["hewey", "dewey", "louie"];
-	
+
 	// build the html string for a <ul> list
-	var items_html = "<ul>";
+	var items_html = "<div>";
 	for (var i=0; i < items.length; i++) {
 		item = items[i];
-		items_html += "<li>" + item + "</li>";
+		items_html += "<p>" + item + "</p>";
 	};
-	items_html += "</ul>";
-	
+	items_html += "</div>";
+
 	// using javascript
 	// 1. find the content div
 	// 2. modify its html attribute by adding items_html
+	var list = document.getElementById("content");
+	list.innerHTML = items_html;
 
-}
-
+	}
