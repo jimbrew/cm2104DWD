3544651b30c4b6b6184feae53aa8f14acc0eda39
JAMES STEVENSON <0703983@rgu.ac.uk>
2018-03-01 11:38:28 +0000

    Added extra columns

--- lab10/roverparser.js
+++ lab10/roverparser.js
@@ -20,11 +20,13 @@
   console.log(result.photos);
   var photos = result.photos //this data contains an array called photos
 
-  var output="<table><thead><tr><th>Rover</th><th>Camera</th><th>Image URL</th></thead><tbody>";
+  var output="<table><thead><tr><th>Rover</th><th>Camera</th><th>Earth Date</th><th>Launch Date</th><th>Image URL</th></thead><tbody>";
   for (var i in photos)
   {
     output+="<tr><td>" + photos[i].rover.name + "</td><td>"
     + photos[i].camera.full_name + "</td><td>"
+    + photos[i].earth_date + "</td><td>"
+    + photos[i].rover.launch_date + "</td><td>"
     + photos[i].img_src + "</td></tr>";
   }
   output+="</tbody></table>";
