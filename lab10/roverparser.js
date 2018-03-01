bc0636d460e75d09d22c5b2718e8be4bf0d1f11d
James Stevenson <0703983@rgu.ac.uk>
2018-03-01 16:15:05 +0000

    Changed img tags JS

--- lab10/roverparser.js
+++ lab10/roverparser.js
@@ -5,36 +5,37 @@
  * @Last modified by:   john
  * @Last modified time: 23-Feb-182018
  */
- $(document).ready(function () {
+$(document).ready(function() {
 
-  $('#retrieve-data').click(function () {
-  var displayResources = $('#display-data');
+  $('#retrieve-data').click(function() {
+    var displayResources = $('#display-data');
 
-  displayResources.text('Loading Rover Data');
+    displayResources.text('Loading Rover Data');
 
-  $.ajax({
-  type: "GET",
-  url: "roverdata.json",
-  success: function(result)
-  {
-  console.log(result.photos);
-  var photos = result.photos //this data contains an array called photos
+    $.ajax({
+      type: "GET",
+      url: "roverdata.json",
+      success: function(result) {
+        console.log(result.photos);
+        var photos = result.photos //this data contains an array called photos
 
-  var output="<table><thead><tr><th>Rover</th><th>Camera</th><th>Earth Date</th><th>Launch Date</th><th>Image URL</th></thead><tbody>";
-  for (var i in photos)
-  {
-    output+="<tr><td>" + photos[i].rover.name + "</td><td>"
-    + photos[i].camera.full_name + "</td><td>"
-    + photos[i].earth_date + "</td><td>"
-    + photos[i].rover.launch_date + "</td><td>"
-    + "<img src=" + photos[i].img_src + "></img></td></tr>";
-  }
-  output+="</tbody></table>";
+        var output = "<table><thead><tr><th>Rover</th><th>Camera</th><th>Earth Date</th><th>Launch Date</th><th>Image URL</th></thead><tbody>";
+        for (var i in photos) {
+          output += "<tr><td>" + photos[i].rover.name + "</td><td>" +
+            photos[i].camera.full_name + "</td><td>" +
+            photos[i].earth_date + "</td><td>" +
+            photos[i].rover.launch_date + "</td><td>" +
+            "<img src=" + photos[i].img_src + " /></td></tr>";
+        }
+        output += "</tbody></table>";
 
-  displayResources.html(output);
-  $("table").addClass("table");
-  }
-  });
+        displayResources.html(output);
+        $("table").addClass("table");
+      }
+    });
 
   });
- });
+
+
+
+});
