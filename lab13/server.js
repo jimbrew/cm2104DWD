a489912e939e77f3ab7e17919f98733b22adbcdd
James Stevenson <0703983@rgu.ac.uk>
2018-03-12 19:52:59 +0000

    added parseInt

--- lab13/server.js
+++ lab13/server.js
@@ -12,8 +12,8 @@ app.get('/', function(req, res) {
           res.send(randomJoke.body);
         });
         app.get('/add', function(req, res) {
-              var x = req.query.x;
-              var y = req.query.y;
+              var x = parseInt(req.query.x);
+              var y = parseInt(req.query.y);
               res.send("X + Y ="+(x+y));
             });
 app.listen(8080);
