29de87c6bb45e340ba813abc7e2140aa0b44361a
James Stevenson <0703983@rgu.ac.uk>
2018-03-12 20:03:50 +0000

    added calc
    added a calculation function to the code

--- lab13/server.js
+++ lab13/server.js
@@ -1,19 +1,25 @@
 var express = require('express');
 var app = express();
 app.get('/', function(req, res) {
-      res.send("Hello world! by express");
-    });
-    app.get('/test', function(req, res) {
-          res.send("Route 2");
-        });
-    app.get('/joke', function(req, res) {
-          var oneLinerJoke = require('one-liner-joke');
-          var randomJoke = oneLinerJoke.getRandomJoke();
-          res.send(randomJoke.body);
-        });
-        app.get('/add', function(req, res) {
-              var x = parseInt(req.query.x);
-              var y = parseInt(req.query.y);
-              res.send("X + Y ="+(x+y));
-            });
+  res.send("Hello world! by express");
+});
+app.get('/test', function(req, res) {
+  res.send("Route 2");
+});
+app.get('/joke', function(req, res) {
+  var oneLinerJoke = require('one-liner-joke');
+  var randomJoke = oneLinerJoke.getRandomJoke();
+  res.send(randomJoke.body);
+});
+app.get('/add', function(req, res) {
+  var x = parseInt(req.query.x);
+  var y = parseInt(req.query.y);
+  res.send("X + Y =" + (x + y));
+});
+app.get('/calc', function(req, res) {
+  var x = parseInt(req.query.x);
+  var y = parseInt(req.query.y);
+  var z = req.query.z;
+  if(z==add){res.send("X + Y =" + (x + y));}
+});
 app.listen(8080);
