9c4f4513d358fb1d1dd4c13d4f0c9e80bacac00f
James Stevenson <0703983@rgu.ac.uk>
2018-03-12 19:43:42 +0000

    addition stuff
    added addition to the server.js file

--- lab13/server.js
+++ lab13/server.js
@@ -9,6 +9,11 @@ app.get('/', function(req, res) {
     app.get('/joke', function(req, res) {
           var oneLinerJoke = require('one-liner-joke');
           var randomJoke = oneLinerJoke.getRandomJoke();
-          res.end(randomJoke.body);
+          res.send(randomJoke.body);
         });
+        app.get('/add', function(req, res) {
+              var x = req.query.x;
+              var y = req.query.y;
+              res.send("X + Y ="+(x+y));
+            });
 app.listen(8080);
