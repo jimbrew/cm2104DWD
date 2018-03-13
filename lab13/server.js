f76fc1996eb7c957810eb335ec005849e5922e9a
jimbrew <35928790+jimbrew@users.noreply.github.com>
2018-03-13 10:06:14 +0000

    Express parameters

--- lab13/server.js
+++ lab13/server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
+app.get('/getform', function(req, res){ 
+var name = req.query.name;
+var quest = req.query.quest;
+   res.send("Hi "+name+" I am sure you will "+quest) ;
+});
 app.get('/', function(req, res) {
   res.send("Hello world! by express");
 });
