36370895a9c613cf645b484bd0d5bd4af18c48fe
James Stevenson <0703983@rgu.ac.uk>
2018-03-14 18:17:12 +0000

    twitter app

--- lab14/server.js
+++ lab14/server.js
@@ -16,7 +16,14 @@ app.get('/', function(req, res){
 
   client.get('statuses/user_timeline', params, function(error, tweets, response){
     if (!error) {
-      res.send(tweets);
+      var output = "";
+      for(var t=0; t<tweets.length; t++){
+        output += "<div>";
+        output += "<h2>" + tweets[t].user.screen_name + "</h2>";
+        output += "<p>" + tweets[t].text + "</p>";
+        output += "</div>";
+      }
+      res.send(output);
     }
   });
   //res.send("hello world by express");
